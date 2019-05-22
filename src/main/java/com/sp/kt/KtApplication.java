f31fcba2bf44d0436df8c146d1c548081e0cd592
unknown <yaabbccj@163.com>
2019-05-22 15:34:41 +0800

    init

---
+++ src/main/java/com/sp/kt/KtApplication.java
@@ -0,0 +1,13 @@
+package com.sp.kt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtApplication.class, args);
+    }
+
+}
